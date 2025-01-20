d02a43ac344f017689987b077ad698f60619dca0
Ginie <github@ginie.eu>
2025-01-20 19:36:27 +0100

    added more graphics and simplified items-header

--- apps/web/src/ui/elements/items-header.svelte
+++ apps/web/src/ui/elements/items-header.svelte
@@ -2,171 +2,124 @@
   interface Props {
     isDouble?: boolean
     variant?: 'white' | 'rare' | 'unique' | 'magic' | 'currency' | 'gem' | 'quest'
+    children?: () => any
   }
 
-  const { isDouble, variant = 'white' }: Props = $props()
+  const { isDouble, variant = 'white', children  }: Props = $props()
 </script>
 
-<div class="host {variant}" class:double={isDouble}>
-  <div class="background"></div>
-  <div class="content">Hello World</div>
+<div class="items-header {variant}" class:double={isDouble}>
+  {@render children?.()}
 </div>
 
 <!-- svelte-ignore css_unused_selector -->
 <style>
-  .host {
+  .items-header {
     --ih-h: 2rem;
     --ih-w: 1.5rem;
     --ih-bg-l: url('/images/itemsheaderwhiteleft.webp');
     --ih-bg-m: url('/images/itemsheaderwhitemiddle.webp');
     --ih-bg-r: url('/images/itemsheaderwhiteright.webp');
   }
-  .host.double {
+  .items-header.double {
     --ih-h: 3rem;
     --ih-w: 2.5rem;
   }
 
-  .host.white,
-  .host.normal {
+  .items-header.white,
+  .items-header.normal {
     --ih-bg-l: url('/images/itemsheaderwhiteleft.webp');
     --ih-bg-m: url('/images/itemsheaderwhitemiddle.webp');
     --ih-bg-r: url('/images/itemsheaderwhiteright.webp');
   }
-  .host.white.double,
-  .host.normal.double {
+  .items-header.white.double,
+  .items-header.normal.double {
     --ih-bg-l: url('/images/itemsheaderdoublenormalleft.webp');
     --ih-bg-m: url('/images/itemsheaderdoublenormalmiddle.webp');
     --ih-bg-r: url('/images/itemsheaderdoublenormalright.webp');
   }
 
-  .host.magic {
+  .items-header.magic {
     --ih-bg-l: url('/images/itemsheadermagicleft.webp');
     --ih-bg-m: url('/images/itemsheadermagicmiddle.webp');
     --ih-bg-r: url('/images/itemsheadermagicright.webp');
   }
-  .host.magic.double {
+  .items-header.magic.double {
     --ih-bg-l: url('/images/itemsheaderdoublemagicleft.webp');
     --ih-bg-m: url('/images/itemsheaderdoublemagicmiddle.webp');
     --ih-bg-r: url('/images/itemsheaderdoublemagicright.webp');
   }
 
-  .host.rare {
+  .items-header.rare {
     --ih-bg-l: url('/images/itemsheaderraresinglelineleft.webp');
     --ih-bg-m: url('/images/itemsheaderraresinglelinemiddle.webp');
     --ih-bg-r: url('/images/itemsheaderraresinglelineright.webp');
   }
-  .host.rare.double {
+  .items-header.rare.double {
     --ih-bg-l: url('/images/itemsheaderrareleft.webp');
     --ih-bg-m: url('/images/itemsheaderraremiddle.webp');
     --ih-bg-r: url('/images/itemsheaderrareright.webp');
   }
 
-  .host.unique {
+  .items-header.unique {
     --ih-bg-l: url('/images/itemsheaderuniquesinglelineleft.webp');
     --ih-bg-m: url('/images/itemsheaderuniquesinglelinemiddle.webp');
     --ih-bg-r: url('/images/itemsheaderuniquesinglelineright.webp');
   }
-  .host.unique.double {
+  .items-header.unique.double {
     --ih-bg-l: url('/images/itemsheaderuniqueleft.webp');
     --ih-bg-m: url('/images/itemsheaderuniquemiddle.webp');
     --ih-bg-r: url('/images/itemsheaderuniqueright.webp');
   }
 
-  .host.currency {
+  .items-header.currency {
     --ih-bg-l: url('/images/itemsheadercurrencyleft.webp');
     --ih-bg-m: url('/images/itemsheadercurrencymiddle.webp');
     --ih-bg-r: url('/images/itemsheadercurrencyright.webp');
   }
-  .host.currency.double {
+  .items-header.currency.double {
     --ih-bg-l: url('/images/itemsheaderdoublecurrencyleft.webp');
     --ih-bg-m: url('/images/itemsheaderdoublecurrencymiddle.webp');
     --ih-bg-r: url('/images/itemsheaderdoublecurrencyright.webp');
   }
 
-  .host.gem {
+  .items-header.gem {
     --ih-bg-l: url('/images/itemsheadergemleft.webp');
     --ih-bg-m: url('/images/itemsheadergemmiddle.webp');
     --ih-bg-r: url('/images/itemsheadergemright.webp');
   }
-  .host.gem.double {
+  .items-header.gem.double {
     --ih-bg-l: url('/images/itemsheaderdoublegemleft.webp');
     --ih-bg-m: url('/images/itemsheaderdoublegemmiddle.webp');
     --ih-bg-r: url('/images/itemsheaderdoublegemright.webp');
   }
 
-  .host.quest {
+  .items-header.quest {
     --ih-bg-l: url('/images/itemsheaderquestleft.webp');
     --ih-bg-m: url('/images/itemsheaderquestmiddle.webp');
     --ih-bg-r: url('/images/itemsheaderquestright.webp');
   }
-  .host.quest.double {
+  .items-header.quest.double {
     --ih-bg-l: url('/images/itemsheaderdoublequestleft.webp');
     --ih-bg-m: url('/images/itemsheaderdoublequestmiddle.webp');
     --ih-bg-r: url('/images/itemsheaderdoublequestright.webp');
   }
 
-  .host {
+  .items-header {
     position: relative;
     box-sizing: border-box;
     text-align: center;
     line-height: 1;
     height: var(--ih-h);
 
-    background-image: var(--ih-bg-m);
-    background-repeat: repeat-x;
-    background-size: auto 100%;
-    background-position: calc(50% - var(--ih-w) / 2) 50%;
+    background-image: var(--ih-bg-l), var(--ih-bg-r), var(--ih-bg-m), var(--ih-bg-m) ;
+    background-repeat: no-repeat, no-repeat, repeat-x , repeat-x;
+    background-size: auto 100%, auto 100%, auto 100%, auto 100%;
+    background-position: left center, right center, 50% 50%, calc(50% - var(--ih-w) / 2) 50%;
 
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-  }
-  .background {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-
-    background-image: var(--ih-bg-m);
-    background-repeat: repeat-x;
-    background-size: auto 100%;
-    background-position: 50% 50%;
-    z-index: 1;
-    user-select: none;
-  }
-  .background::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    z-index: 1;
-    background-image: var(--ih-bg-l);
-    background-repeat: no-repeat;
-    background-size: auto 100%;
-    background-position: left;
-    width: var(--ih-w);
-    mask-image: linear-gradient(to right, rgba(0, 0, 0, 1) 90%, transparent 95%);
-  }
-  .background::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 1;
-    background-image: var(--ih-bg-r);
-    background-repeat: no-repeat;
-    background-size: auto 100%;
-    background-position: right;
-    width: var(--ih-w);
-    mask-image: linear-gradient(to left, rgba(0, 0, 0, 1) 90%, transparent 95%);
-  }
-
-  .content {
-    position: relative;
-    z-index: 2;
   }
 </style>

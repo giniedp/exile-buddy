4b9ec3d5a5a3ba1c27e2e877baddf74e88360053
Ginie <github@ginie.eu>
2025-02-02 00:00:44 +0100

    restored goto in layout

--- apps/web/src/routes/db/npcs/+layout.svelte
+++ apps/web/src/routes/db/npcs/+layout.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
-  import { recordIdFromSlug } from '$lib/utils'
+  import { goto } from '$app/navigation'
+  import { recordIdFromSlug, recordIdToSlug } from '$lib/utils'
   import { NpcTable } from '$lib/widgets/npcs'
   let { data, children } = $props()
   let selection = $state(recordIdFromSlug(data.id))
+  $effect(() => {
+    if (selection) {
+      goto(`/db/npcs/${recordIdToSlug(selection)}`)
+    }
+  })
 </script>
 
 <div class="flex h-full flex-row overflow-hidden">
   <div class="flex flex-auto flex-col overflow-auto">
-    <NpcTable data={data.items} {selection} />
+    <NpcTable data={data.items} bind:selection={selection} />
   </div>
   <div>
     {@render children()}

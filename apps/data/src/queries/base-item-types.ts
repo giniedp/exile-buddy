06c0f4b2294d0af2ad50c30f07a93c515de09d71
themixednuts <jonfonts@gmail.com>
2025-02-03 18:45:26 -0600

    update baseItemTypes queries

--- apps/data/src/queries/base-item-types.ts
+++ apps/data/src/queries/base-item-types.ts
@@ -9,18 +9,17 @@ export async function findBaseItemTypes(db: Poe2Database) {
 export async function findBaseItemTypeById(db: Poe2Database, id: string) {
   return await db.query.baseItemTypes.findFirst({
     where: (items, { eq }) => eq(items.id, id),
+    columns: {
+      itemVisualIdentity: false,
+      itemClass: false,
+    },
     with: {
-      armourTypes: true,
-      attributeRequirements: true,
-      uncutGemAdditionalTiers: true,
-      delveCraftingModifiers: true,
       itemClass: {
         with: {
           itemClassCategory: true,
         },
       },
       itemVisualIdentity: true,
-      itemInherentSkills: true,
     },
   })
 }
@@ -28,18 +27,17 @@ export async function findBaseItemTypeById(db: Poe2Database, id: string) {
 export async function findBaseItemTypeByClassIdx(db: Poe2Database, classIdx: number) {
   return await db.query.baseItemTypes.findMany({
     where: (items, { eq }) => eq(items.itemClass, classIdx),
+    columns: {
+      itemVisualIdentity: false,
+      itemClass: false,
+    },
     with: {
-      // armourTypes: true,
-      // attributeRequirements: true,
-      // uncutGemAdditionalTiers: true,
-      // delveCraftingModifiers: true,
-      // itemClass: {
-      //   with: {
-      //     itemClassCategory: true,
-      //   },
-      // },
+      itemClass: {
+        with: {
+          itemClassCategory: true,
+        },
+      },
       itemVisualIdentity: true,
-      // itemInherentSkills: true,
     },
   })
 }

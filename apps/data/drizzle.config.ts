ae2296b60f83a0f082dc216242bbb8f8423ca0e7
themixednuts <jonfonts@gmail.com>
2025-02-04 07:27:08 -0600

    feat: drizzle-studio

--- apps/data/drizzle.config.ts
+++ apps/data/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/db/schema.ts',
+  schema: './src/generated/schema.ts',
   dialect: 'sqlite',
   dbCredentials: {
     url: 'file:../web/static/cdn/poe2.db',
--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
+  out: '$data/drizzle',
+  schema: '$data/src/generated/schema.ts',
   dialect: 'sqlite',
   dbCredentials: {
     url: 'file:static/cdn/poe2.db',
